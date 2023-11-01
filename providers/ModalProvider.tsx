92c952f97dfcb60cacbf6ad0102fd22fff798d19
Dananjay 996 <sreedananjay456@gmail.com>
2023-11-01 18:28:57 +0530

    Added Provider for Auth and Upload - 01-11-2023

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React from 'react'
+
+import AuthModal from '@/components/AuthModal';
+import UploadModal from '@/components/UploadModal';
+
+const ModalProvider = () => {
+
+  const [isMounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <AuthModal />
+      <UploadModal />
+    </>
+  )
+}
+
+export default ModalProvider
