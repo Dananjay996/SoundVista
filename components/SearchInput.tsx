d77693bb492535cc1098cd64d5d283edf21e3307
Dananjay 996 <sreedananjay456@gmail.com>
2023-11-13 19:47:11 +0530

    Added Input bar in search page - 10-11-2023

---
+++ components/SearchInput.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import React from "react"; 
+import qs from "query-string"
+import { useRouter } from "next/navigation";
+
+import useDebounce from "@/hooks/useDebounce";
+import Input from "./Input";
+
+const SearchInput = () => {
+    const router = useRouter();
+    const [value,setValue] = React.useState<string>("");
+    const debouncedValue = useDebounce<string>(value,500)
+
+    React.useEffect(() => {
+        const query = {title: debouncedValue} 
+
+        const url = qs.stringifyUrl({
+            url: '/search',
+            query : query
+        })
+
+        router.push(url)
+    },[debouncedValue,router])
+
+    return(
+        <Input placeholder="What do you want to search?" value={value} onChange={(e) => setValue(e.target.value)} />
+    )
+}
+
+export default SearchInput;
